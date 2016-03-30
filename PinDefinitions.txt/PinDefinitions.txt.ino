7d2338e54da33e4bd47cecf2aef24bb70d20ebcd
Matt <amicableskeptic@gmail.com>
2016-03-30 11:21:46 -0400

    Adding file to document what pins are used for

---
+++ PinDefinitions.txt/PinDefinitions.txt.ino
@@ -0,0 +1,30 @@
+Pins
+Analog
+A0 Outside temp
+A1 Inside Temp
+A2 Battery Sensor
+A3 Solar Sensor
+A4 West Winch Bottom Limit
+A5 West Winch Teop Limit
+A6 East Winch Bottom Limit
+A7 East Winch Top Limit
+
+Digital
+D2 Shutters Direction
+D3 Shutters Power
+D4 uSD X East Winch Dir ISSUE!!!
+D5 East Winch Power
+D6 West Winch Dir X LED Pin ISSUE!!!
+D7 West Winch Power
+D8 PIR Sensor
+D9 Heat Wave Switch
+D10 Fan Power
+D11 uSD
+D12 uSD
+D13 uSD
+
+
+Could D2 be an issue?
+    // on a fully populated WildFire v3
+    // Dig22 (CC3000 interrupt) and Dig2 (RFM69 interrupt)
+
